c12a07953dbdee8b20234358839054f2306f1427
forbearing <fredjoseph863@gmail.com>
2021-07-16 13:44:13 +0800

    stage three script

---
+++ scripts/3_stage_three.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+source /etc/os-release
+case "$ID" in
+    centos|rhel)
+        stage_one_script_path="centos/1_prepare_for_server.sh"
+        stage_two_script_path="centos/2_prepare_for_k8s.sh"
+        stage_three_script_path="centos/3_install_docker.sh" ;;
+    ubuntu)
+        stage_one_script_path="ubuntu/1_prepare_for_server.sh"
+        stage_two_script_path="ubuntu/2_prepare_for_k8s.sh"
+        stage_three_script_path="ubuntu/3_install_docker.sh" ;;
+    debian)
+        stage_one_script_path="debian/1_prepare_for_server.sh"
+        stage_two_script_path="debian/2_prepare_for_k8s.sh"
+        stage_three_script_path="debian/3_install_docker.sh" ;;
+    *)
+        ERR "Not Support Linux !" && exit $EXIT_FAILURE ;;
+esac
+
+function stage_three {
+    for NODE in "${ALL_NODE[@]}"; do
+        ssh "${NODE}" "bash -s" < "${stage_three_script_path}"
+    done
+}
