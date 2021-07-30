ba838807b9d21156e3d2623d0459f00e8aabe050
forbearing <fredjoseph863@gmail.com>
2021-07-30 22:37:22 +0800

    k8s csi plugin for ceph rbd

--- addons-3rd/csi-ceph/get-ceph-cluster-id.sh
+++ addons-3rd/csi-ceph/get-ceph-cluster-id.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-#ceph_admin=10.230.20.11
-ssh root@$ceph_admin ceph -s | grep "id:" | awk '{print $2}'
+ceph_mon_ip=10.250.20.11
+ssh root@$ceph_node_ip ceph -s | grep "id:" | awk '{print $2}'
