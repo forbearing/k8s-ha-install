83ff0c4910a3b7898864269ad20058d21883352d
forbearing <fredjoseph863@gmail.com>
2021-08-12 10:58:43 +0800

    deploy k8s cluster script

--- setup.sh
+++ setup.sh
@@ -54,96 +54,51 @@ ERR(){ echo -e "\033[31m\033[01m$1\033[0m"; }
 MSG1(){ echo -e "\n\n\033[32m\033[01m$1\033[0m\n"; }
 MSG2(){ echo -e "\n\033[33m\033[01m$1\033[0m"; }
 
-# k8s and etcd path
-K8S_PATH="/etc/kubernetes"
-KUBE_CERT_PATH="/etc/kubernetes/pki"
-ETCD_CERT_PATH="/etc/etcd/ssl"
+K8S_PATH="/etc/kubernetes"                              # k8s config path
+KUBE_CERT_PATH="/etc/kubernetes/pki"                    # k8s cert path
+ETCD_CERT_PATH="/etc/etcd/ssl"                          # etcd cert path
 K8S_DEPLOY_LOG_PATH="/root/k8s-deploy-log"              # k8s install log dir path
-INSTALL_MANAGER=""                                      # like apt-get, yum etc, not set here
+INSTALL_MANAGER=""                                      # like apt-get yum, setting by script, not set here
+environment_file=""                                     # default k8s environment file is k8s.env
 
-environment_file=""
-while getopts "e:h" opt; do
+source scripts/function.sh                              # base function script
+source scripts/0_stage_prepare.sh                       # deploy k8s cluster stage prepare script
+source scripts/1_stage_one.sh                           # deploy k8s cluster stage one script
+source scripts/2_stage_two.sh                           # deploy k8s cluster stage two script
+source scripts/3_stage_three.sh                         # deploy k8s cluster stage three script
+source scripts/4_stage_four.sh                          # deploy k8s cluster stage four script
+source scripts/5_stage_five.sh                          # deploy k8s cluster stage five script
+source scripts/add-k8s-node.sh                          # add k8s worker node script
+source scripts/del-k8s-node.sh                          # del k8s worker node script
+
+while getopts "e:ad:h" opt; do
     case "${opt}" in
         e) environment_file="${OPTARG}" ;;
-        h) MSG1 "$(basename $0) -e environment_file" && exit $EXIT_SUCCESS ;;
-        *) ERR "$(basename $0) -e environment_file" && exit $EXIT_FAILURE
+        a) i_want_add_k8s_node="true" ;;
+        d) i_want_del_k8s_node="true";
+           DEL_WORKER="${OPTARG}" ;;
+        h) usage; exit $EXIT_SUCCESS ;;
+        *) usage; exit $EXIT_FAILURE ;;
     esac
 done
-#[ -z $environment_file ] && ERR "$(basename $0) -e environment_file" && exit $EXIT_FAILURE
-[ -z $environment_file ] && environment_file="k8s.env"
-source "$environment_file"
+[[ ${environment_file} ]] || environment_file="k8s.env"
+source ${environment_file}
 ALL_NODE=( ${!MASTER[@]} ${!WORKER[@]} )
 
 
-function print_environment {
-    MSG1 "=================================== Environment ==================================="
-    MSG2 "master node"
-    for HOST in "${!MASTER[@]}"; do
-        local IP=${MASTER[$HOST]}
-        printf "%-20s%s\n" ${HOST} ${IP}; done
-
-    MSG2 "worker node"
-    for HOST in "${!WORKER[@]}"; do
-        local IP=${WORKER[$HOST]}
-        printf "%-20s%s\n" ${HOST} ${IP}; done
-
-    MSG2 "all k8s node"
-    for HOST in "${ALL_NODE[@]}"; do
-        echo ${HOST}; done
-
-    MSG2 "extra master node"
-    for HOST in "${!EXTRA_MASTER[@]}"; do
-        local IP=${EXTRA_MASTER[$HOST]}
-        printf "%-20s%s\n" ${HOST} ${IP}; done
-
-    MSG2 "add worker node"
-    for HOST in "${!ADD_WORKER[@]}"; do
-        local IP=${ADD_WORKER[$HOST]}
-        printf "%-20s%s\n" ${HOST} ${IP}; done
-
-    MSG2 "others environment"
-    echo "CONTROL_PLANE_ENDPOINT:   ${CONTROL_PLANE_ENDPOINT}"
-    echo "SRV_NETWORK_CIDR:         ${SRV_NETWORK_CIDR[*]}"
-    echo "SRV_NETWORK_IP:           ${SRV_NETWORK_IP}"
-    echo "SRV_NETWORK_DNS_IP:       ${SRV_NETWORK_DNS_IP[*]}"
-    echo "POD_NETWORK_CIDR:         ${POD_NETWORK_CIDR[*]}"
-    echo "K8S_PATH                  ${K8S_PATH}"
-    echo "KUBE_CERT_PATH:           ${KUBE_CERT_PATH}"
-    echo "ETCD_CERT_PATH:           ${ETCD_CERT_PATH}"
-    MSG1 "=================================== Environment ==================================="
-}
-function check_root_and_os() {
-    # 检测是否为支持的 Linux 版本，否则退出脚本
-    # 检测是否为 root 用户，否则推出脚本
-    [[ "$(uname)" != "Linux" ]] && ERR "Not Support OS !" && exit $EXIT_FAILURE
-    [[ $(id -u) -ne 0 ]] && ERR "Not ROOT !" && exit $EXIT_FAILURE
-    source /etc/os-release
-    if [[ "$ID" == "centos" || "$ID" == "rhel" ]]; then
-        INSTALL_MANAGER="yum"
-    elif [[ "$ID" == "debian" || "$ID" == "ubuntu" ]]; then
-        INSTALL_MANAGER="apt-get"
-    else
-        ERR "Not Support Linux !"
-        EXIT $EXIT_FAILURE
-    fi
-    # 检查网络是否可用，否则退出脚本
-    if ! timeout 2 ping -c 2 -i 1 114.114.114.114 &> /dev/null; then ERR "no network" && exit $EXIT_FAILURE; fi
-}
+[[ ${i_want_add_k8s_node} ]] && add_k8s_node && exit ${EXIT_SUCCESS}
+[[ ${i_want_del_k8s_node} ]] && del_k8s_node && exit ${EXIT_SUCCESS}
 
 
 check_root_and_os
 print_environment
-source scripts/0_stage_prepare.sh
-source scripts/1_stage_one.sh
-source scripts/2_stage_two.sh
-source scripts/3_stage_three.sh
-source scripts/4_stage_four.sh
-source scripts/5_stage_five.sh
 
-MSG1 "=============  Stage Prepare: Setup SSH Public Key Authentication ============="; stage_prepare
-MSG1 "=================== Stage 1: Prepare for Linux Server ========================="; stage_one
-MSG1 "====================== Stage 2: Prepare for Kubernetes ========================"; stage_two
-MSG1 "========================= Stage 3: Install Docker ============================="; stage_three
-MSG1 "============ Stage 4: Deployment Kubernetes Cluster from Binary ==============="; stage_four
-MSG1 "==================== Stage 5: Deployment Kubernetes Addon ====================="; stage_five
-MSG1 "NOT Forget Restart All Kubernetes Node !!!"
+function main {
+    stage_prepare
+    stage_one
+    stage_two
+    stage_three
+    stage_four
+    stage_five
+    MSG1 "NOT Forget Restart All Kubernetes Node !!!"
+}; main
