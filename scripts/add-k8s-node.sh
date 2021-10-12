9ed2db73070e191d26ba06159427d64ce5bbe3b7
forbearing <fredjoseph863@gmail.com>
2021-10-12 23:27:30 +0800

    add k8s node script

--- scripts/add-k8s-node.sh
+++ scripts/add-k8s-node.sh
@@ -144,12 +144,14 @@ function 4_run_stage_one {
             MSG2 "*** ${NODE} *** is Preparing for Linux Server"
             ssh root@${NODE} \
                 "export TIMEZONE=${TIMEZONE}
+                 $(typeset -f _apt_wait)
                  $(typeset -f 1_upgrade_system)
                  $(typeset -f 2_install_necessary_package)
                  $(typeset -f 3_disable_firewald_and_selinux)
                  $(typeset -f 4_set_timezone_and_ntp_client)
                  $(typeset -f 5_configure_sshd)
                  $(typeset -f 6_configure_ulimit)
+                 _apt_wait
                  1_upgrade_system
                  2_install_necessary_package
                  3_disable_firewald_and_selinux
@@ -205,9 +207,11 @@ function 5_run_stage_two {
         for NODE in "${ADD_WORKER[@]}"; do
             MSG2 "*** ${NODE} *** is Preparing for Kubernetes"
             ssh root@${NODE} \
-                "$(typeset -f 1_disable_swap)
+                "$(typeset -f _apt_wait)
+                 $(typeset -f 1_disable_swap)
                  $(typeset -f 2_load_kernel_module)
                  $(typeset -f 3_configure_kernel_parameter)
+                 _apt_wait
                  1_disable_swap
                  2_load_kernel_module
                  3_configure_kernel_parameter" \
@@ -256,9 +260,11 @@ function 6_run_stage_three {
             MSG2 "*** ${NODE} *** is Installing Docker"
             ssh root@${NODE} \
                 "export TIMEZONE=${TIMEZONE}
+                 $(typeset -f _apt_wait)
                  $(typeset -f 1_install_docker)
                  $(typeset -f 2_configure_docker)
                  $(typeset -f 3_audit_for_docker)
+                 _apt_wait
                  1_install_docker
                  2_configure_docker
                  3_audit_for_docker" \
