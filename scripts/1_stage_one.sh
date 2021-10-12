5fd696662420cfb8f2e4df10157ec02b5a659ff7
forbearing <fredjoseph863@gmail.com>
2021-10-12 23:26:28 +0800

    stage one script

--- scripts/1_stage_one.sh
+++ scripts/1_stage_one.sh
@@ -39,12 +39,14 @@ function stage_one {
             MSG2 "*** ${NODE} *** is Preparing for Linux Server"
             ssh root@${NODE} \
                 "export TIMEZONE=${TIMEZONE}
+                 $(typeset -f _apt_wait)
                  $(typeset -f 1_upgrade_system)
                  $(typeset -f 2_install_necessary_package)
                  $(typeset -f 3_disable_firewald_and_selinux)
                  $(typeset -f 4_set_timezone_and_ntp_client)
                  $(typeset -f 5_configure_sshd)
                  $(typeset -f 6_configure_ulimit)
+                 _apt_wait
                  1_upgrade_system
                  2_install_necessary_package
                  3_disable_firewald_and_selinux
