37645d312382e03ae13ec379c679882bb6850664
forbearing <fredjoseph863@gmail.com>
2021-12-25 21:14:01 +0800

    support rocky linux

--- scripts/1_stage_one.sh
+++ scripts/1_stage_one.sh
@@ -32,6 +32,32 @@ function stage_one {
         MSG2 "Please Waiting... (multitail -s 3 -f ${K8S_DEPLOY_LOG_PATH}/logs/stage-one/*.log)"
         wait
         ;;
+    rocky)
+        # Linux: rocky
+        source rocky/1_prepare_for_server.sh
+        for NODE in "${ALL_NODE[@]}"; do
+            MSG2 "*** ${NODE} *** is Preparing for Linux Server"
+            ssh root@${NODE} \
+                "export TIMEZONE=${TIMEZONE}
+                 $(typeset -f 1_import_repo)
+                 $(typeset -f 2_install_necessary_package)
+                 $(typeset -f 3_upgrade_system)
+                 $(typeset -f 4_disable_firewald_and_selinux)
+                 $(typeset -f 5_set_timezone_and_ntp_client)
+                 $(typeset -f 6_configure_sshd)
+                 $(typeset -f 7_configure_ulimit)
+                 1_import_repo
+                 2_install_necessary_package
+                 3_upgrade_system
+                 4_disable_firewald_and_selinux
+                 5_set_timezone_and_ntp_client
+                 6_configure_sshd
+                 7_configure_ulimit" \
+                 &> ${K8S_DEPLOY_LOG_PATH}/logs/stage-one/${NODE}.log &
+        done
+        MSG2 "Please Waiting... (multitail -s 3 -f ${K8S_DEPLOY_LOG_PATH}/logs/stage-one/*.log)"
+        wait
+        ;;
     ubuntu)
         # Linux: ubuntu
         source ubuntu/1_prepare_for_server.sh
