c6128c93436b0110a06f714ee38a74e6abe3bc89
forbearing <fredjoseph863@gmail.com>
2021-12-26 11:07:06 +0800

    support debian 11

--- scripts/1_stage_one.sh
+++ scripts/1_stage_one.sh
@@ -86,7 +86,29 @@ function stage_one {
         ;;
     debian)
         # Linux: debian
-        :
+        source debian/1_prepare_for_server.sh
+        for NODE in "${ALL_NODE[@]}"; do
+            MSG2 "*** ${NODE} *** is Preparing for Linux Server"
+            ssh root@${NODE} \
+                "export TIMEZONE=${TIMEZONE}
+                 $(typeset -f _apt_wait)
+                 $(typeset -f 1_upgrade_system)
+                 $(typeset -f 2_install_necessary_package)
+                 $(typeset -f 3_disable_firewald_and_selinux)
+                 $(typeset -f 4_set_timezone_and_ntp_client)
+                 $(typeset -f 5_configure_sshd)
+                 $(typeset -f 6_configure_ulimit)
+                 _apt_wait
+                 1_upgrade_system
+                 2_install_necessary_package
+                 3_disable_firewald_and_selinux
+                 4_set_timezone_and_ntp_client
+                 5_configure_sshd
+                 6_configure_ulimit" \
+                 &> ${K8S_DEPLOY_LOG_PATH}/logs/stage-one/${NODE}.log &
+        done
+        MSG2 "Please Waiting... (multitail -s 3 -f ${K8S_DEPLOY_LOG_PATH}/logs/stage-one/*.log)"
+        wait
         ;;
     *)
         ERR "Not Support Linux !" && exit $EXIT_FAILURE ;;
