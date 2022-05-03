318627e2a969edad88a47c2aef2072ee3ad1568e
hybfkuf <fredjoseph863@gmail.com>
2022-05-03 14:09:34 +0800

    setup linux software repository

--- scripts/0_stage_prepare.sh
+++ scripts/0_stage_prepare.sh
@@ -16,57 +16,77 @@
 
 prepare_software_mirror() {
     if [[ $TIMEZONE == "Asia/Shanghai" || $TIMEZONE == "Asia/Chongqing" ]]; then
+        local mirror
+        local isRockyMirror
+        # Official Archive Mirrors for Ubuntu
+        # https://launchpad.net/ubuntu/+archivemirrors
+        # 1. ustc 使用太多会限速甚至是连不上,先测试下 nju
+        case ${LINUX_SOFTWARE_MIRROR,,} in
+        nju)      mirror="http://mirror.nju.edu.cn"; isRockyMirror=1 ;;     # 南京大学, 10Gbps
+        bupt)     mirror="http://mirrors.bupt.edu.cn" ;;                    # 北京邮电大学, 10Gbps
+        ustc)     mirror="http://mirrors.ustc.edu.cn"; isRockyMirror=1 ;;   # 中国科技技术大学, 10Gbps
+        aliyun)   mirror="http://mirrors.aliyun.com" ;;                     # 阿里云, 2Gbps
+        tencent)  mirror="http://mirrors.cloud.tencent.com"; isRockyMirror=1 ;; # 腾讯云, 2Gbps
+        sjtu)     mirror="http://ftp.sjtu.edu.cn" ;;                        # 上海交通大学, 1Gbps
+        bjtu)     mirror="http://mirror.bjtu.edu.cn" ;;                     # 北京交通大学, 1Gbps
+        dlut)     mirror="http://mirror.dlut.edu.cn" ;;                     # 大连理工大学, 1Gbps
+        hit)      mirror="http://mirrors.hit.edu.cn" ;;                     # 哈尔滨工业大学, 1Gbps
+        huawei)   mirror="http://repo.huaweicloud.com" ;;                   # 华为云, 1Gbps
+        njupt)    mirror="http://mirrors.njupt.edu.cn" ;;                   # 南京邮电大学, 1Gbps
+        sohu)     mirror="http://mirrors.sohu.com" ;;                       # 搜狐, 1Gbps
+        xjtu)     mirror="http://mirrors.xjtu.edu.cn"; isRockyMirror=1 ;;   # 西安交通大学, 1Gbps
+        skyshe)   mirror="http://mirrors.skyshe.cn"; isRockyMirror=1 ;;     # xTom open source software, 1Gbps
+        lzu)      mirror="http://mirror.lzu.edu.cn" ;;                      # 兰州大学, 100Mbps
+        cqu)      mirror="http://mirrors.cqu.edu.cn" ;;                     # 重庆大学, 100Mbps
+        dgut)     mirror="http://mirrors.dgut.edu.cn" ;;                    # 东莞理工学院, 100Mbps
+        tsinghua) mirror="http://mirrors.tuna.tsinghua.edu.cn" ;;           # 清华大学
+        bfsu)     mirror="http://mirrors.bfsu.edu.cn" ;;                    # 北京外国语大学
+        163)      mirror="http://mirrors.163.com" ;;                        # 网易
+        *)        mirror="http://mirror.nju.edu.cn" ;;                      # 南京大学, 10Gbps
+        esac
+        [ $isRockyMirror ] || mirror="http://mirror.nju.edu.cn"
+
         case $linuxID in
-        ubuntu|debian)
-            # Official Archive Mirrors for Ubuntu
-            # https://launchpad.net/ubuntu/+archivemirrors
-            # 1. ustc 使用太多会限速甚至是连不上,先测试下 nju
-            case ${LINUX_SOFTWARE_MIRROR,,} in
-            nju)      mirror="http://mirror.nju.edu.cn/$linuxID" ;;          # 南京大学, 10Gbps
-            bupt)     mirror="http://mirrors.bupt.edu.cn/$linuxID" ;;        # 北京邮电大学, 10Gbps
-            ustc)     mirror="http://mirrors.ustc.edu.cn/$linuxID" ;;        # 中国科学技术大学, 10Gbps
-            aliyun)   mirror="http://mirrors.aliyun.com/$linuxID" ;;         # 阿里云, 2Gbps
-            tencent)  mirror="http://mirrors.cloud.tencent.com/$linuxID" ;;  # 腾讯云, 2Gbps
-            sjtu)     mirror="http://ftp.sjtu.edu.cn/$linuxID" ;;            # 上海交通大学, 1Gbps
-            bjtu)     mirror="http://mirror.bjtu.edu.cn/$linuxID" ;;         # 北京交通大学, 1Gbps
-            dlut)     mirror="http://mirror.dlut.edu.cn/$linuxID" ;;         # 大连理工大学, 1Gbps
-            hit)      mirror="http://mirrors.hit.edu.cn/$linuxID" ;;         # 哈尔滨工业大学, 1Gbps
-            huawei)   mirror="http://repo.huaweicloud.com/$linuxID" ;;       # 华为云, 1Gbps
-            njupt)    mirror="http://mirrors.njupt.edu.cn/$linuxID" ;;       # 南京邮电大学, 1Gbps
-            sohu)     mirror="http://mirrors.sohu.com/$linuxID" ;;           # 搜狐, 1Gbps
-            xjtu)     mirror="http://mirrors.xjtu.edu.cn/$linuxID" ;;        # 西安交通大学, 1Gbps
-            skyshe)   mirror="http://mirrors.skyshe.cn/$linuxID" ;;          # xTom open source software, 1Gbps
-            lzu)      mirror="http://mirror.lzu.edu.cn/$linuxID" ;;          # 兰州大学, 100Mbps
-            cqu)      mirror="http://mirrors.cqu.edu.cn/$linuxID" ;;         # 重庆大学, 100Mbps
-            dgut)     mirror="http://mirrors.dgut.edu.cn/$linuxID" ;;        # 东莞理工学院, 100Mbps
-            tsinghua) mirror="http://mirrors.tuna.tsinghua.edu.cn/$linuxID" ;; # 清华大学
-            bfsu)     mirror="http://mirrors.bfsu.edu.cn/$linuxID" ;;        # 北京外国语大学
-            163)      mirror="http://mirrors.163.com/$linuxID" ;;            # 网易
-            *)        [ $linuxID == "ubuntu" ] && mirror="http://cn.archive.ubuntu.com/$linuxID"
-                      [ $linuxID == "debian" ] && mirror="ftp.cn.debian.org/$linuxID" ;;
-            esac
-            [ $linuxID == "ubuntu" ] && \
-                source_list=(
-                    "deb $mirror $linuxCodeName main restricted universe multiverse"
-                    "deb $mirror $linuxCodeName-security main restricted universe multiverse"
-                    "deb $mirror $linuxCodeName-updates main restricted universe multiverse"
-                    "deb $mirror $linuxCodeName-proposed main restricted universe multiverse"
-                    "deb $mirror $linuxCodeName-backports main restricted universe multiverse"
-                    "deb-src $mirror $linuxCodeName main restricted universe multiverse"
-                    "deb-src $mirror $linuxCodeName-security main restricted universe multiverse"
-                    "deb-src $mirror $linuxCodeName-updates main restricted universe multiverse"
-                    "deb-src $mirror $linuxCodeName-proposed main restricted universe multiverse"
-                    "deb-src $mirror $linuxCodeName-backports main restricted universe multiverse")
-            [ $linuxID == "debian" ] && \
-                source_list=(
-                    "deb $mirror ${linuxCodeName} main non-free contrib"
-                    "deb $mirror ${linuxCodeName}-updates main non-free contrib"
-                    "deb $mirror-security ${linuxCodeName}-security main non-free contrib"
-                    "deb-src $mirror ${linuxCodeName} main non-free contrib"
-                    "deb-src $mirror ${linuxCodeName}-updates main non-free contrib"
-                    "deb-src $mirror-security ${linuxCodeName}-security main non-free contrib")
-            cp -f /etc/apt/sources.list /etc/apt/sources.list.$(date +%Y%m%d%H%M)
+        debian)
+            source_list=(
+                "deb $mirror/$linuxID ${linuxCodeName} main non-free contrib"
+                "deb $mirror/$linuxID ${linuxCodeName}-updates main non-free contrib"
+                "deb $mirror/$linuxID-security ${linuxCodeName}-security main non-free contrib"
+                "#deb-src $mirror/$linuxID ${linuxCodeName} main non-free contrib"
+                "#deb-src $mirror/$linuxID ${linuxCodeName}-updates main non-free contrib"
+                "#deb-src $mirror/$linuxID-security ${linuxCodeName}-security main non-free contrib")
+            yes | cp /etc/apt/sources.list /etc/apt/sources.list.$(date +%Y%m%d%H%M)
             printf "%s\n" "${source_list[@]}" > /etc/apt/sources.list
+            ;;
+        ubuntu)
+            source_list=(
+                "deb $mirror/$linuxID $linuxCodeName main restricted universe multiverse"
+                "deb $mirror/$linuxID $linuxCodeName-security main restricted universe multiverse"
+                "deb $mirror/$linuxID $linuxCodeName-updates main restricted universe multiverse"
+                "deb $mirror/$linuxID $linuxCodeName-proposed main restricted universe multiverse"
+                "deb $mirror/$linuxID $linuxCodeName-backports main restricted universe multiverse"
+                "#deb-src $mirror/$linuxID $linuxCodeName main restricted universe multiverse"
+                "#deb-src $mirror/$linuxID $linuxCodeName-security main restricted universe multiverse"
+                "#deb-src $mirror/$linuxID $linuxCodeName-updates main restricted universe multiverse"
+                "#deb-src $mirror/$linuxID $linuxCodeName-proposed main restricted universe multiverse"
+                "#deb-src $mirror/$linuxID $linuxCodeName-backports main restricted universe multiverse")
+            yes | cp /etc/apt/sources.list /etc/apt/sources.list.$(date +%Y%m%d%H%M)
+            printf "%s\n" "${source_list[@]}" > /etc/apt/sources.list
+            ;;
+        rocky)
+            [ $linuxMajorVersion == "8" ] && \
+                sed -r -e "s|^mirrorlist=|#mirrorlist=|g" \
+                    -e "s|^#baseurl=http|baseurl=http|g" \
+                    -e "s|baseurl=(.*)releasever|baseurl=$mirror/rocky/\$releasever|g" \
+                    -i.$(date +%Y%m%d%H%M) \
+                    /etc/yum.repos.d/Rocky*.repo 
+            ;;
+        centos)
+            sed -r -e "s|^mirrorlist=|#mirrorlist=|g" \
+                -e "s|^#baseurl=http|baseurl=http|g" \
+                -e "s|baseurl=(.*)releasever|baseurl=$mirror/centos/\$releasever|g" \
+                -i.$(date +%Y%m%d%H%M) \
+                /etc/yum.repos.d/CentOS-*.repo
             ;;
         esac
     fi
@@ -89,9 +109,12 @@ _stage_prepare() {
         _apt_wait && \
             apt-get update -y && \
             apt-get install -y sshpass multitail ;;
-    rocky|centos)
-        yum install -y epel-release
-        yum install -y sshpass multitail ;;
+    rocky)
+        yum localinstall -y rocky/pkgs/multitail-6.5.0-1.el${linuxMajorVersion}.x86_64.rpm
+        yum localinstall -y rocky/pkgs/sshpass-1.06-9.el${linuxMajorVersion}.x86_64.rpm ;;
+    centos)
+        yum localinstall -y centos/pkgs/multitail-6.5.0-1.el$linuxMajorVersion.x86_64.rpm
+        yum install -y sshpass ;;
     *)
         ERR "Not Support Linux $linuxID!"
         exit $EXIT_FAILURE
@@ -140,12 +163,17 @@ _stage_prepare() {
     case $linuxID in
     centos)
         for node in "${ALL_NODE[@]}"; do
-            scp -q -r centos/yum.repos.d $node:/tmp/ &
+            scp -q -r centos/pkgs centos/yum.repos.d $node:/tmp/ &
         done
         wait; ;;
     rocky)
         for node in "${ALL_NODE[@]}"; do
-            scp -q -r rocky/yum.repos.d $node:/tmp/ &
+            scp -q -r rocky/pkgs rocky/yum.repos.d $node:/tmp/ &
+        done
+        wait; ;;
+    ubuntu)
+        for node in "${ALL_NODE[@]}"; do
+            scp -q -r ubuntu/pkgs $node:/tmp/ &
         done
         wait; ;;
     esac
